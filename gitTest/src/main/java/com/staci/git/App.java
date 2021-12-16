785acb146b42cc98361be2f424aa0d8f02b1239a
mchaabani <mchaabani@16STALA025-iaul.mshome.net>
2021-12-16 14:34:53 +0100

    Test MCH

--- gitTest/src/main/java/com/staci/git/App.java
+++ gitTest/src/main/java/com/staci/git/App.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
-        //TEst
+        //SpringApplication.run(App.class, args);
+        
+    	System.out.println("Hello Word......");
+    	
     }
 }
